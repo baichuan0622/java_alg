8a7d44c99e00d731ae20167e974851446f5344c0
baichuan0622 <kevin0622bc@163.com>
2016-02-06 17:55:29 +0800

    Alorithms

---
+++ src/Chaper_2/Quick.java
@@ -0,0 +1,12 @@
+package Chaper_2;
+
+/**
+ * Created by Kevin on 2016/2/6.
+ */
+public class Quick {
+
+    public static void sort()
+    {
+        System.out.print("1");
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,10 +1,12 @@
+import Chaper_2.Quick;
+
 /**
  * Created by Kevin on 2016/2/6.
  */
 public class main {
     public static void main(String[] args)
     {
-        System.out.print("1");
+        Quick.sort();
     }
 
 }
